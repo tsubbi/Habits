f0a5e2108196e8d4d9752614912acf4f7f2de7fe
Jamie <jamie.yjchen@gmail.com>
2021-06-09 12:10:39 -0700

    init project

---
+++ Habits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Habits
+//
+//  Created by Jamie Chen on 2021-06-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
